46b6ead40a44bc5f79784b0d791fcd75d92458c5
fmolinalopez <fmolinalopezp@gmail.com>
2018-02-19 13:31:34 +0100

    Añadidos ejemplos de eventos y configuraciones

--- crearDropdown.js
+++ crearDropdown.js
@@ -1,3 +1,25 @@
+Dropzone.options.dropzone = {
+    init: function() {
+        this.on("addedfile", function(file) {
+            console.log(file.name);
+            alert(`Añadido archivo ${file.name}`);
+        });
+        this.on("dragenter", function (file) {
+            console.log(`El archivo ha entrado en la dropzone`)
+        });
+        this.on("dragleave", function (file) {
+            console.log(`El archivo ha salido de la dropzone`)
+        });
+    },
+    accept: function(file, done) {
+        if (file.name === "test.txt") {
+            done("No puedes subir archivos llamados test.txt");
+        }
+        else { done(); }
+    },
+    clickable: false,
+};
+
 function crearDropdown() {
     let container = document.getElementById("container"); // Div contenedor
     let div = document.createElement("div"); // Crea un div que sera el dropzone
@@ -5,4 +27,11 @@ function crearDropdown() {
     div.setAttribute("class", "dropzone"); // Le asigna una clase al div
     container.appendChild(div); // Asigna el div dropzone al div contenedor
     let myDropzone = new Dropzone("div#miDiv", { url: "upload.php"}); // Convierte el div en dropzone
+    myDropzone.on("complete", function (file) {
+        console.log(file.name);
+        myDropzone.removeFile(file);
+    })
+    myDropzone.on("removedfile", function (file) {
+        alert(`Se ha eliminado el archivo ${file.name} de la dropzone`);
+    })
 }
--- upload.php
+++ upload.php
@@ -1,16 +1,16 @@
 <?php
-$ds          = DIRECTORY_SEPARATOR;  //1
+$ds          = DIRECTORY_SEPARATOR;  //1 Guarda la constante DIRECTORY_SEPARATOR en una variable corta por comodidad.
 
-$storeFolder = 'uploads';   //2
+$storeFolder = 'uploads';   //2 Carpeta donde se guardaran los archivos.
 
-if (!empty($_FILES)) {
+if (!empty($_FILES)) { // Si recibe un archivo
 
-    $tempFile = $_FILES['file']['tmp_name'];          //3
+    $tempFile = $_FILES['file']['tmp_name'];         //3 Guarda el archivo en una variable temporal.
 
-    $targetPath = dirname( __FILE__ ) . $ds. $storeFolder . $ds;  //4
+    $targetPath = dirname( __FILE__ ) . $ds. $storeFolder . $ds;  //4 Crea la ruta a la carpeta de destino.
 
-    $targetFile =  $targetPath. $_FILES['file']['name'];  //5
+    $targetFile =  $targetPath. $_FILES['file']['name'];  //5 Crea la ruta al destino del archivo.
 
-    move_uploaded_file($tempFile,$targetFile); //6
+    move_uploaded_file($tempFile,$targetFile); //6 Mueve el archivo a la carpeta.
 
 }
